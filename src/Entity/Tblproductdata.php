474646c11fd02f97c2d0983903c772df20bc6be0
asaider <rodion.kovalev@mail.ru>
2019-04-11 20:17:41 +0300

    create migrations

---
+++ src/Entity/Tblproductdata.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tblproductdata
+ *
+ * @ORM\Table(name="tblProductData", uniqueConstraints={@ORM\UniqueConstraint(name="strProductCode", columns={"strProductCode"})})
+ * @ORM\Entity
+ */
+class Tblproductdata
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="intProductDataId", type="integer", nullable=false, options={"unsigned"=true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $intproductdataid;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="strProductName", type="string", length=50, nullable=false)
+     */
+    private $strproductname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="strProductDesc", type="string", length=255, nullable=false)
+     */
+    private $strproductdesc;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="strProductCode", type="string", length=10, nullable=false)
+     */
+    private $strproductcode;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="stock", type="integer", options={"unsigned"=true})
+     */
+    private $stock;
+
+    /**
+     * @var decimal
+     *
+     * @ORM\Column(name="price", type="decimal", precision=10, scale=2)
+     */
+    private $price;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="dtmAdded", type="datetime", nullable=true)
+     */
+    private $dtmadded;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="dtmDiscontinued", type="datetime", nullable=true)
+     */
+    private $dtmdiscontinued;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="stmTimestamp", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
+     */
+    private $stmtimestamp = 'CURRENT_TIMESTAMP';
+
+
+}
---
+++ src/Migrations/Version20190411165051.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190411165051 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE tblProductData (intProductDataId INT UNSIGNED AUTO_INCREMENT NOT NULL, strProductName VARCHAR(50) NOT NULL, strProductDesc VARCHAR(255) NOT NULL, strProductCode VARCHAR(10) NOT NULL, dtmAdded DATETIME DEFAULT NULL, dtmDiscontinued DATETIME DEFAULT NULL, stmTimestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, UNIQUE INDEX strProductCode (strProductCode), PRIMARY KEY(intProductDataId)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE tblProductData');
+    }
+}
